86878dcf877c7b207275e1cec238eb504fe04058
Vincent Taverna <vinnymac@gmail.com>
2013-12-14 04:54:00 -0500

    WIP move to gradle

---
+++ bittool/build/source/buildConfig/debug/com/vinnymac/bittool/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.vinnymac.bittool;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+
+}
